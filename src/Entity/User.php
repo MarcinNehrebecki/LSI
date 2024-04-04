42811b031d66ee7dd128af6135d2399d4ae58025
Marcin Nehrebecki <marcin.nehrebecki@gmail.com>
2024-04-05 01:39:32 +0200

    Add dates to user entity.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,6 +16,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    use DataEntityTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
